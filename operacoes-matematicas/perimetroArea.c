7042061494ad04a4f29eb0ddad2c5a8040b6f5be
Thalia Silva <thalia105@hotmail.com>
2025-05-09 16:59:38 -0300

    Add files via upload

---
+++ operacoes-matematicas/perimetroArea.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+int main(){
+
+float a,b, c, diferente, perimetro, area; 
+
+scanf("%f %f %f", &a, &b, &c);
+ a- b= diferente;
+if (diferente >= c )
+{
+   area = (a + b) * c / 2 ;
+    printf("Area = %.1f", area);
+}
+else 
+{
+    a + b + c = perimetro;
+
+    printf("Perimetro = %.1f",perimetro);
+}
+
+
+
+    return 0;
+}
